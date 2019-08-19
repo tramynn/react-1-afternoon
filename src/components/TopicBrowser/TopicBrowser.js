d54cc77bd7492990ed97b4fba3f5d0202280d9de
Tramy Nguyen <tramynn2@gmail.com>
2019-08-19 14:16:40 -0500

    added App.js and TopicBrowser.js

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class TopicBrowser extends Component {
+    render() {
+        return (
+            <p>Hello World</p>
+        )
+    }
+}
+
