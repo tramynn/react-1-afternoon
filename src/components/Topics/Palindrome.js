c4726eb6865eeaf010d1c3488da1635946cf9231
Tramy Nguyen <tramynn2@gmail.com>
2019-08-19 19:08:42 -0500

    completed filter string

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 export default class Palindrome extends Component {
     render() {
         return (
-            <p> Palindrome Component </p>
+            <div className="puzzleBox palindromePB"> 
+                <h4>Palindrome Component</h4> 
+                <span></span>
+                <input></input>
+                <button></button>
+                <span></span>
+            </div>
         )
     }
 }
